eeb2afae707b3d19f6272e7cc04a718d6705a4a0
unknown <79253649+thawchj@users.noreply.github.com>
2021-08-04 18:28:03 +0700

    commit

---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="body">
+    <p class="text1">พวกเราพร้อมส่งสินค้า</p>
+    <p class="text2">เพื่อคุณตลอดเวลา</p>
+    <NuxtLink to="/pop">
+      <button class="bt">พบกันเร็วๆ นี้</button>
+    </NuxtLink>
+    
+  </div>
+</template>
+
+<script>
+export default {
+    
+     
+}
+
+</script>
+
+<style scoped>
+.body{
+  margin-left: 300px;
+ 
+}
+.text1{
+  margin-top: 348px;
+  margin-right: 936px;
+  font-size: 58px;
+  font-weight: bold;
+}
+.text2{
+  margin-bottom: 58px;
+  font-size: 45px;
+}
+.bt{
+  margin-bottom: 422px;
+  background-color: rgb(63, 63, 63);
+  color: white;
+  font-size: 45px;
+  padding: 10px 30px;
+  border-radius: 15px;
+  border: none
+
+}
+</style>
---
+++ pages/pop.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="body">
+        <p class="text-selectprovince">เลือกจังหวัด</p>
+        <select v-model="province" class="combobox">
+            <option value="" selected disabled>กรุณาเลือกจังหวัด</option>
+            <option  v-for="p in provinceArr" :key="p.province_code" >{{p.province_name_th}}</option>
+        </select>
+        <div class="d-flex justify-content-center">
+            <button class="bt ">ตกลง</button>
+        </div>
+        <div class="box d-flex justify-content-center align-items-center">
+            <p class="text-province">{{province}}</p>
+        </div>
+        
+    </div>
+    
+</template>
+
+<script>
+
+export default {
+  async asyncData({ $axios }) {
+        const res = await $axios.$get(process.env.API)
+        return {
+            provinceArr: res.result.data
+        }
+    },
+    data(){
+        return{
+            provinceArr: null,
+            province: '',
+        }
+    }, 
+    methods:{
+        show(){
+
+        }
+    } 
+}
+</script>
+
+<style scoped>
+.body{
+    margin: 50px 123px 0px 123px;
+}
+.combobox{
+    font-size: 30px;
+    width: 100%;
+}
+.text-selectprovince{
+    font-size: 30px;
+}
+.bt{
+  background-color: rgb(63, 63, 63);
+  color: white;
+  font-size: 45px;
+  padding: 5px 5vw;
+  border-radius: 15px;
+  border: none;
+  margin: 46px 0px;
+}
+.text-province{
+    font-size: 45px;
+}
+.box{
+    border: 0.5px solid black;
+    min-height: 200px;
+}
+</style>
